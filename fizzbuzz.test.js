7be273fa5cd999cc8eee4a0faa8aa0614c025466
Gascon <xavier.gascon@accenture.com>
2023-03-20 16:17:56 +0100

    3 tests

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,35 @@
+// *3 -> fizz, *5 -> buzz; *3&*5 -> fizzbuzz
+
+function fizzbuzz(nombre){
+    if(nombre === 3){
+        return 'fizz'
+    }
+    return nombre
+};
+
 describe("fizzbuzz", () => {
-    it("test", () => {
-        expect(true).toBe(true)
+    it("Pour 1 renvoie 1", () => {
+        // Given
+        const nombre = 1
+        // When
+        const result = fizzbuzz(nombre)
+        // Then
+        expect(result).toEqual(1)
+    })
+    it("Pour 2 renvoie 2", () => {
+        // Given
+        const nombre = 2
+        // When
+        const result = fizzbuzz(nombre)
+        // Then
+        expect(result).toEqual(2)
+    })
+    it("Pour 3 renvoie fizz", () => {
+        // Given
+        const nombre = 3
+        // When
+        const result = fizzbuzz(nombre)
+        // Then
+        expect(result).toEqual('fizz')
     })
 })
